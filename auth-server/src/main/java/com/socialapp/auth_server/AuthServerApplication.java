6bfbc4014ec948fb854bac1a9968cb017d4ab17f
aj-octy <aj.octy@gmail.com>
2025-07-08 11:23:02 +0530

    all yml files of microservices in config server and maintain through git with encrypted data

--- auth-server/src/main/java/com/socialapp/auth_server/AuthServerApplication.java
+++ auth-server/src/main/java/com/socialapp/auth_server/AuthServerApplication.java
@@ -1,9 +1,12 @@
 package com.socialapp.auth_server;
 
+import com.socialapp.auth_server.dto.AuthServerContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AuthServerContactInfoDto.class)
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
---
+++ auth-server/src/main/java/com/socialapp/auth_server/Info.java
@@ -0,0 +1,31 @@
+package com.socialapp.auth_server;
+
+import com.socialapp.auth_server.dto.AuthServerContactInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class Info {
+
+        @Value("${build.version}")
+        private String buildVersion;
+
+        @Autowired
+        private AuthServerContactInfoDto authServerContactInfoDto;
+
+        @GetMapping("/build-info")
+        public ResponseEntity<String> getBuildInfo() {
+                return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+        }
+
+        @GetMapping("/contact-info")
+        public ResponseEntity<AuthServerContactInfoDto> getContactInfo() {
+                return ResponseEntity.ok(authServerContactInfoDto);
+        }
+}
